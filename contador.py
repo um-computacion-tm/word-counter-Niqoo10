1bad925c956f4eab9cb6919a2d2b44fe43888098
Niqoo10 <nicopanella2009@hotmail.com>
2023-04-04 15:57:37 -0300

    Contador

--- contador.py
+++ contador.py
@@ -1,30 +1,13 @@
-import unittest
 
-#PARTE DEL CODIGO
 def count_word(texto):
     result = {}
 
-    palabras = texto.lower().split()
+    palabras = texto.lower().split() #Cambia las mayusculas por minusculas
     for palabra in palabras:
         if palabra in result:
-            result[palabra] += 1
+            result[palabra] += 1 #Si existe la palabra, la suma
         else:
-            result[palabra] = 1
+            result[palabra] = 1 #Si no existe la palabra, la agrega
 
     return result
 
-class TestCountWord(unittest.TestCase):
-    def test_palabra_simple(self):
-        result = count_word("Hola")
-        self.assertEqual(result, {'hola': 1})
-
-    def test_cadena_vacia(self):
-        result = count_word("")
-        self.assertEqual(result, {})
-
-    def test_mayusculas(self):
-        result = count_word("Hola MUNDO. hola")
-        self.assertEqual(result, {'hola': 2, 'mundo.': 1})
-    
-if __name__ == '__main__':
-    unittest.main()
